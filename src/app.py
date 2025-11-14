0e9e662554980c66200bae129274bb54419e3b3f
frederick ohene <66055564+frederickohe@users.noreply.github.com>
2025-11-14 09:18:24 +0000

    db creation fix

--- src/app.py
+++ src/app.py
@@ -58,24 +58,22 @@ app = FastAPI(
     },
 )
 
+def tables_exist():
+    inspector = inspect(engine)
+    existing_tables = inspector.get_table_names()
+    return len(existing_tables) > 0
 
+# Initialize database tables only if they don't exist
+if not tables_exist():
+    print("Initializing database tables...")
+    Base.metadata.create_all(bind=engine)
+    print("Database tables initialized successfully.")
+else:
+    print("Database tables already exist.")
 
-# def tables_exist():
-#     inspector = inspect(engine)
-#     existing_tables = inspector.get_table_names()
-#     return len(existing_tables) > 0
-
-# # Initialize database tables only if they don't exist
-# if not tables_exist():
-#     print("Initializing database tables...")
-#     Base.metadata.create_all(bind=engine)
-#     print("Database tables initialized successfully.")
-# else:
-#     print("Database tables already exist.")
-
-print("Initializing database tables...")
-Base.metadata.create_all(bind=engine)
-print("Database tables initialized successfully.")
+# print("Initializing database tables...")
+# Base.metadata.create_all(bind=engine)
+# print("Database tables initialized successfully.")
 
 # Add middleware for CORS
 app.add_middleware(
